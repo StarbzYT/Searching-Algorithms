7302d132681f511c1efc4a06fbd7527d00299efa
Adarsh <chhokaradarsh@gmail.com>
2020-08-07 14:46:31 -0700

    added linear search algo

---
+++ linear_search.py
@@ -0,0 +1,19 @@
+# linear search function; time complexity: O(n)
+
+
+def linear(arr, val):
+    i = 0  # track of indices
+    while i < len(arr):
+        for item in arr:  # go through each index in arr
+            if item == val:
+                return i
+            i += 1
+        return -1  # if val not found...
+
+
+print(linear([10, 15, 20, 25], 15))  # 1
+print(linear([200], 100))  # -1
+
+
+
+
